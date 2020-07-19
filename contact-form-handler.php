3ebb66f1184c5782ce772017ac580856d3e1d00c
johng034 <67410822+johng034@users.noreply.github.com>
2020-07-19 02:03:12 -0700

    Last try with the contact form

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,16 +1,25 @@
 <?php
 
-if (isset($_POST['submit'])) {
     $name = $_POST['name'];
-    $mailFrom = $_POST['email'];
+    $email = $_POST['email'];
     $message = $_POST['message'];
 
-    $mailTo = 'johng2055@yahoo.com';
-    $headers = "From: ".$mailFrom;
-    $txt = "You have received a message from ".$name.".\n\n".$message;
-    
-    mail($mailTo, $txt, $headers);
+    $email_from = 'JohnG@johng034.github.io';
+
+    $email_subject = "New Message from Portfolio Website";
+
+    $email_body = "Name: $name.\n".
+                    "Email: $email.\n".
+                    "Message: $message.\n";
+
+    $to = "johng2055@yahoo.com";
+
+    $headers = "From: $email_from \r\n";
+
+    $headers .= "Reply-To: $email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
     header("Location: index.html");
-}
 
 ?>
