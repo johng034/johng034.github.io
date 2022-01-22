95d6811672bb3048082908dd21861866c8a60a35
johng034 <67410822+johng034@users.noreply.github.com>
2022-01-21 19:20:25 -0800

    added summary to two projects and some more javascript code

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,8 +1,23 @@
-function readMore() {
+function languageReadMore() {
     var dots = document.getElementById("dots0");
-    // var tag = dots.getAttributeNames
     var moreText = document.getElementById("languageMore");
     var btnText = document.getElementById("languageReadMore");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
+
+function twitterReadMore() {
+    var dots = document.getElementById("dots1");
+    var moreText = document.getElementById("twitterMore");
+    var btnText = document.getElementById("twitterReadMore");
 
     if (dots.style.display === "none") {
         dots.style.display = "inline";
