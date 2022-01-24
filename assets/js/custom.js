9fed72d450c6a1e64ba92316a0c6685bddf647f2
johng034 <67410822+johng034@users.noreply.github.com>
2022-01-23 22:27:30 -0800

    added description to mit books project

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -29,3 +29,19 @@ function twitterReadMore() {
         moreText.style.display = "inline";
     }
 }
+
+function booksReadMore() {
+    var dots = document.getElementById("dots3");
+    var moreText = document.getElementById("booksMore");
+    var btnText = document.getElementById("booksReadMore");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
