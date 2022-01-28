3484d6cf35ce7db67db86329db6a7d7a03d94fac
johng034 <67410822+johng034@users.noreply.github.com>
2022-01-27 16:14:59 -0800

    removed color change in background and link to elements page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -30,6 +30,22 @@ function twitterReadMore() {
     }
 }
 
+function customerReadMore() {
+    var dots = document.getElementById("dots2");
+    var moreText = document.getElementById("customerMore");
+    var btnText = document.getElementById("customerReadMore");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
+
 function booksReadMore() {
     var dots = document.getElementById("dots3");
     var moreText = document.getElementById("booksMore");
